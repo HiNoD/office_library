cff0eef930f2737a2f051ebdf02fc0c9a977dc4b
Hemul997 <nikitamoshkin1997@gmail.com>
2019-04-22 22:46:01 +0300

    Добавил класс для работы с api

---
+++ office_library/src/domain/repository/OfiiceLibraryServer.ts
@@ -0,0 +1,35 @@
+import axios, {AxiosError, AxiosPromise, AxiosRequestConfig, AxiosResponse} from 'axios';
+
+class OfficeLibraryServer {
+  public BASE_URL = '';
+
+  public get<T = any>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const axiosPromise = axios.get(`${this.BASE_URL}${url}`, config);
+    return this.handleServerResponse(axiosPromise);
+  }
+
+  public delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const axiosPromise = axios.delete(`${this.BASE_URL}${url}`, config);
+    return this.handleServerResponse(axiosPromise);
+  }
+
+  public post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+    const axiosPromise = axios.post(`${this.BASE_URL}${url}`, data, config);
+    return this.handleServerResponse(axiosPromise);
+  }
+
+  public put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+    const axiosPromise = axios.put(`${this.BASE_URL}${url}`, data, config);
+    return this.handleServerResponse(axiosPromise);
+  }
+
+  private async handleServerResponse<T = any>(axiosPromise: AxiosPromise<T>): Promise<T> {
+    return axiosPromise
+      .then((response: AxiosResponse) => {
+        return response.data;
+      }, (error: AxiosError) => {
+        return error;
+      });
+  }
+}
+export default new OfficeLibraryServer();
