bf1ae343ad796c16cb2e3abb12880086bd74c51d
Hemul997 <nikitamoshkin1997@gmail.com>
2019-05-05 20:14:36 +0300

    Delete unnecessary tabs

--- office_library/src/components/NavigationPanel/NavigationPanel.tsx
+++ office_library/src/components/NavigationPanel/NavigationPanel.tsx
@@ -6,20 +6,20 @@ import {
 } from "react-router-dom"
 
 export class NavigationPanel extends React.Component {
-    public render() {
-        return (
-          <ul className="App-navigation-buttons nav flex-column">
-            <NavLink exact={true} to="/">
-              <li className="nav-item"><p className="App-navigation-button-text">Список книг</p></li>
-            </NavLink>
-            <NavLink to="/users">
-              <li className="nav-item"><p className="App-navigation-button-text">Список пользователей</p></li>
-            </NavLink>
-            <NavLink to="/orders">
-              <li className="nav-item"><p className="App-navigation-button-text">Заявки</p></li>
-            </NavLink>
-          </ul>
-        
-        )
-    }
+  public render() {
+    return (
+      <ul className="App-navigation-buttons nav flex-column">
+        <NavLink exact={true} to="/">
+          <li className="nav-item"><p className="App-navigation-button-text">Список книг</p></li>
+        </NavLink>
+        <NavLink to="/users">
+          <li className="nav-item"><p className="App-navigation-button-text">Список пользователей</p></li>
+        </NavLink>
+        <NavLink to="/orders">
+          <li className="nav-item"><p className="App-navigation-button-text">Заявки</p></li>
+        </NavLink>
+      </ul>
+    
+    )
+  }
 }
