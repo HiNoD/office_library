24833b1da3e98cc952d1e98189617650da64b87c
Hemul997 <nikitamoshkin1997@gmail.com>
2019-05-07 10:22:03 +0300

    Fixes by adding user form

--- office_library/src/components/AddingUserForm/AddingUserForm.tsx
+++ office_library/src/components/AddingUserForm/AddingUserForm.tsx
@@ -1,31 +1,52 @@
-import * as React from "react";
-import { Field, Form } from 'react-final-form';
+import * as React from 'react';
+
+import { Field, Form} from 'react-final-form';
+
+import { Grid, Paper } from '@material-ui/core'
 
 const onSubmit = async (values:any) => {
   // await sleep(300)
   window.alert(JSON.stringify(values, null, 2))
 }
 
-export const AddingUserForm: React.SFC = () => {
-  return (
-    <Form
-      onSubmit={onSubmit} // tslint:disable-next-line: jsx-no-lambda
-      render={({ handleSubmit, form, submitting, pristine, values }) => (
-        <form onSubmit={handleSubmit}>
-        
-          <div>
-            <Field name="lastName" component="input" placeholder="Фамилия" />
-            <Field name="name" component="input" placeholder="Имя" />
-            <Field name="patronymic" component="input" placeholder="Отчество" />
-            <Field name="position" component="input" placeholder="Должность"  />
-            <button type="submit" disabled={submitting || pristine}>
-              +Создать
-            </button>
-          </div>
-          <pre>{JSON.stringify(values, null, 2)}</pre>
-        </form>
-      )}
-    
-    />
-  );
+export class AddingUserForm extends React.Component {
+  public render() {
+    return (
+      <Form
+        onSubmit={onSubmit} // tslint:disable-next-line: jsx-no-lambda
+        render={({ handleSubmit, form, submitting, pristine, values }) => (
+          <form onSubmit={handleSubmit}>
+          
+            <Paper style={{ padding: 16 }}>
+              <Grid item={true} xs={12}>
+                <Field 
+                margin=""
+                fullWidth={true}
+                required={true}
+                name="lastName"
+                component="input"
+                type="text"
+                placeholder="Фамилия" 
+                />
+              </Grid>
+              <Grid item={true} xs={12}>
+                <Field name="name" component="input" placeholder="Имя" />
+              </Grid>
+              <Grid item={true} xs={12}>
+                <Field name="patronymic" component="input" placeholder="Отчество" />
+              </Grid>
+              <Grid item={true} xs={12}>
+                <Field name="position" component="input" placeholder="Должность"  />
+              </Grid>
+              <button type="submit" disabled={submitting || pristine}>
+                +Создать
+              </button>
+            </Paper>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </form>
+        )}
+      
+      />
+    );
+  } 
 };
